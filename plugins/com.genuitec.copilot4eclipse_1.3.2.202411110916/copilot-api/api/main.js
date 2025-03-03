7660e0e2c45336dbbf31604d8802f3989252eeb6
Priya Madhok <priyamadhok@MacBookAir.localdomain>
2025-03-03 14:41:15 -0500

    Updated version:

--- plugins/com.genuitec.copilot4eclipse_1.3.2.202411110916/copilot-api/api/main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const ca_1 = require("./ca");
-// setup certificate authority
-(0, ca_1.setupCa)();
-// load the copilot language server
-const copilotAPI = require("./language-server.js");
